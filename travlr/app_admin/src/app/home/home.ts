42c1132c5c840ee4509de76f1442c3289b387346
Dellrodar <3357168+Dellrodar@users.noreply.github.com>
2026-08-15 15:50:45 -0400

    Add home landing page with destination images - Show a welcome heading and blurb on the root route - Render a row of destination image cards that link to the travel listing

--- travlr/app_admin/src/app/home/home.ts
+++ travlr/app_admin/src/app/home/home.ts
@@ -1,9 +1,16 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
-export class Home {}
+export class Home {
+  destinations = [
+    { name: 'Gale Reef', image: 'reef1.jpg' },
+    { name: 'Dawson’s Reef', image: 'reef2.jpg' },
+    { name: 'Claire’s Reef', image: 'reef3.jpg' },
+  ];
+}
